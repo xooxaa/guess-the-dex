a4d7b338ec30a99ca558e34b1d4bda7c7735829e
xooxaa <37028661+xooxaa@users.noreply.github.com>
2024-12-18 00:20:47 +0100

    add game component; implement game logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,16 +1,45 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { UpperCasePipe } from '@angular/common';
 import { PokeDexService } from '../services/pokedex.service';
 
 @Component({
   selector: 'app-game',
-  imports: [UpperCasePipe],
+  imports: [ReactiveFormsModule, UpperCasePipe],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
 export class GameComponent {
-  private readonly pokeDexService = inject(PokeDexService);
+  readonly pokeDexService = inject(PokeDexService);
   currentPokemon = computed(() => {
     return this.pokeDexService.pokeResource.value();
   });
+
+  guess = new FormControl(0);
+  feedback = signal<string>('');
+  score = signal<number>(0);
+
+  makeGuess() {
+    if (this.guess.value && this.currentPokemon()?.id) {
+      const diff = Math.abs(this.guess.value - this.currentPokemon()!.id);
+
+      if (this.guess.value === this.currentPokemon()?.id) {
+        this.feedback.set(
+          `🎉 Correct! 
+          The Pokédex number is ${this.currentPokemon()?.id}.`
+        );
+      } else {
+        this.feedback.set(
+          `❌ Incorrect. 
+          Your guess is off by ${diff}. 
+          The correct number is ${this.currentPokemon()?.id}.`
+        );
+      }
+    }
+  }
+
+  newPokemon() {
+    this.pokeDexService.fetchRandomPokemon();
+    this.feedback.set('');
+  }
 }
